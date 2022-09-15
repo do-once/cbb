635dbf6f99f0ac9665afc399f1ea3ba4367751ea
BryanAdamss <bryanadamss@foxmail.com>
2022-09-15 19:45:30 +0800

    chore(tool-chain): use right eslint extend config

--- tools/toolchain/.eslintrc.js
+++ tools/toolchain/.eslintrc.js
@@ -10,6 +10,10 @@
 require('@rushstack/eslint-config/patch/modern-module-resolution')
 
 module.exports = {
-  extends: ['@rushstack/eslint-config/profile/node'], // <---- put your profile string here;若为web应用使用@rushstack/eslint-config/profile/web-app；参考https://github.com/microsoft/rushstack/tree/master/eslint/eslint-config#2-choose-one-profile
+  // 若为web应用使用@rushstack/eslint-config/profile/web-app
+  // 若为node工具使用@rushstack/eslint-config/profile/node-trusted-tool
+  // 若为node服务使用@rushstack/eslint-config/profile/node
+  // 参考https://github.com/microsoft/rushstack/tree/master/eslint/eslint-config#2-choose-one-profile
+  extends: ['@rushstack/eslint-config/profile/node-trusted-tool'], // <---- put your profile string here;
   parserOptions: { tsconfigRootDir: __dirname }
 }
