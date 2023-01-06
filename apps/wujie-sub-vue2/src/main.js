e193ee7453ac0ce4b9130a8687f25c2e7015d4c6
BryanAdamss <bryanadamss@foxmail.com>
2023-01-06 15:45:32 +0800

    chore: remove pinia deps

--- apps/wujie-sub-vue2/src/main.js
+++ apps/wujie-sub-vue2/src/main.js
@@ -1,15 +1,11 @@
 import Vue from 'vue'
-import { createPinia, PiniaVuePlugin } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
 
-Vue.use(PiniaVuePlugin)
-
 new Vue({
   router,
-  pinia: createPinia(),
   render: h => h(App)
 }).$mount('#app')
